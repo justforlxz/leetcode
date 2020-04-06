77b9a1f32914376f772c62597e474be3976eabd3
justforlxz <justforlxz@gmail.com>
2020-04-06 14:53:18 +0800

    rust init

---
+++ src/0001.two-num/twoNum.rs
@@ -0,0 +1,5 @@
+mod leetcode {
+    pub fn twoSum(nums&: std::vector<int>, target: i32) -> std::vector<int> {
+
+    }
+}
---
+++ src/0001.two-num/twoNum_tests.rs
@@ -0,0 +1,8 @@
+#[cfg(test)]
+mod tests {
+    use leetcode::twoNum;
+
+    fn twoNum_tests() {
+        assert!(false);
+    }
+}
