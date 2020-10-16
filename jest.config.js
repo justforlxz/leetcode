686fbe3b8949841b1ea4396fd51f5be762e9d7be
Zhang, DingYuan <justforlxz@gmail.com>
2020-10-16 14:13:28 +0800

    add jest-junit

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  clearMocks: true,
-  coverageProvider: 'v8',
-  coverageDirectory: "coverage",
-  preset: 'ts-jest',
-  testEnvironment: "node",
-  testRegex: "(tests/.*|(\\.|/)(test|spec))\\.tsx?$"
-};
